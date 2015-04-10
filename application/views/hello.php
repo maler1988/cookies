8bd8340e1cc696e52fedb64a30a4e2872a3e990e
Alexandr <maler1988@yandex.ru>
2015-04-11 00:52:32 +0400

    Hello World!

---
+++ application/views/hello.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Hello, world!</title>
+</head>
+<body>
+
+<div id="container">
+    <h1>Hello, world!</h1>
+</div>
+
+</body>
+</html>
