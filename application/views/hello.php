f0a86d40428109e9c69e1abd63b3f60839c1cfd1
Alexandr <maler1988@yandex.ru>
2015-04-16 00:48:37 +0400

    Тестовый текст

--- application/views/hello.php
+++ application/views/hello.php
@@ -3,12 +3,14 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Hello, world!</title>
+    <title>Плюшки</title>
 </head>
 <body>
 
-<div id="container">
-    <h1>Hello, world!</h1>
+<div id="container" >
+    <h1>Плюшки</h1>
+    <p>Плюшки - это моя небольшая база знаний в которой содержаться "плюшки" - часто используемые куски код
+    готовые решения, разбор проблемных задач, инструкции по использованию git, putty, .htaccess и т.п.</p>
 </div>
 
 </body>
